33483fbfa4ca3cea85d1e6166772bff062746287
Dev Kalra <kalradev@outlook.com>
2020-08-23 16:12:00 +0530

    go-service

---
+++ go-service/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	pb "greeter-service/greeter-service/greeter"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type GreeterServer struct {
+}
+
+func (*GreeterServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{
+		WelcomeMessage: fmt.Sprintf("Getting Request From %s", req.Name),
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &GreeterServer{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
